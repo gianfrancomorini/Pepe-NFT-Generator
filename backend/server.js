88d2ae1bda7ee37b3fc8a24b97b828372a2f55c0
gianfrancomorini <115338984+gianfrancomorini@users.noreply.github.com>
2024-09-28 09:35:55 -0700

    Removed .ebextensions and updated CORS configuration in server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const { Readable } = require('stream');
 const app = express();
 app.use(cors({
   origin: ['https://gianfrancomorini.github.io', 'http://localhost:3000'],
-  methods: ['GET', 'POST'],
+  methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
